b282e73c7e67e178ccbbc4b93e2ff9b23af728a0
ramzi <ramzeddine.saadi@gmail.com>
2026-04-09 23:27:04 -0400

    Test connexion MySQL

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from voitures import Voiture
 
 v = Voiture("Toyota", "Corolla", 2020, 20000)
 v.afficher_voiture()
+
+from crud_db import connecter_db
+
+conn = connecter_db()
+print("connexion OKEY")
+conn.close()
