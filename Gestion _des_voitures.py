cba7ff3565ec82e3d9e3814e4dac9b52b85b5795
ramzi <ramzeddine.saadi@gmail.com>
2026-04-09 22:15:35 -0400

    Création de projet

---
+++ Gestion _des_voitures.py
@@ -0,0 +1,10 @@
+class Voiture:
+    def __init__(self, marque, modele, annee, prix, id=None):
+        self.id = id
+        self.marque = marque
+        self.modele = modele
+        self.annee = annee
+        self.prix = prix
+
+    def afficher_voiture(self):
+        print(f"ID: {self.id}, Marque: {self.marque}, Modèle: {self.modele}, Année: {self.annee}, Prix: {self.prix}")
