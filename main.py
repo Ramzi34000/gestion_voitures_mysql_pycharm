64fe0d9ef596b471178369d62ea864ebd2aa9d94
ramzi <ramzeddine.saadi@gmail.com>
2026-04-09 23:51:40 -0400

    Insertion voiture

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ from crud_db import connecter_db
 conn = connecter_db()
 print("connexion OKEY")
 conn.close()
+
+from crud_db import ajouter_voiture
+
+v = Voiture("BMW", "M3CS", 2022, 80000)
+ajouter_voiture(v)
