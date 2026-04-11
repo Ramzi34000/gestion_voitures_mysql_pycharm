7f82df29184c4b13976665c6190dd6f0e4150d57
ramzi <ramzeddine.saadi@gmail.com>
2026-04-11 00:58:59 -0400

    Fini

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from voitures import Voiture
-from crud_db import ajouter_voiture, supprimer_voiture, connecter_db
+from crud_db import ajouter_voiture, supprimer_voiture, connecter_db, recuperer_voitures, modifier_voiture
 
 
 v1 = Voiture("Toyota", "Corolla", 2020, 20000)
 ajouter_voiture(v1)
-
 print("Ajout OK")
 
 
@@ -13,14 +12,22 @@ print("Connexion OK")
 conn.close()
 
 
+
 v2 = Voiture("BMW", "M3CS", 2022, 80000)
 ajouter_voiture(v2)
 
 
-supprimer_voiture(1)
+
+v_mod = Voiture("Toyota", "Corolla Sport", 2021, 22000, 1)
+modifier_voiture(v_mod)
+print("Modification OK")
+
+
+
+supprimer_voiture(2)
 print("Suppression OK")
 
-from crud_db import recuperer_voitures
+
 
 voitures = recuperer_voitures()
 
